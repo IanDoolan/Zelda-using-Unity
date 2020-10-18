149a50953c177dd6b655cb9cac5e3f79e43ddb1e
Ian Doolan <iandoolan@gmail.com>
2020-10-18 17:48:13 +0100

    Part 29 Completed
    Transition Fading Effect

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -8,13 +8,43 @@ public class SceneTransition : MonoBehaviour
     public string sceneToLoad;
     public Vector2 playerPosition;
     public VectorValue playerStorage;
+    public GameObject fadeInPanel;
+    public GameObject fadeOutPanel;
+    public float fadeWait;
+
+    // Fade In White Panel
+    private void Awake()
+    {
+        if(fadeInPanel != null)
+        {
+            GameObject panel = Instantiate(fadeInPanel, Vector3.zero, Quaternion.identity) as GameObject;
+            Destroy(panel, 1);
+        }
+    }
 
     public void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.CompareTag("Player") && !other.isTrigger)
         {
             playerStorage.initialValue = playerPosition;
-            SceneManager.LoadScene(sceneToLoad);
+            //SceneManager.LoadScene(sceneToLoad);
+            StartCoroutine(FadeCo());
         }    
+    }
+
+    public IEnumerator FadeCo()
+    {
+        if(fadeOutPanel != null)
+        {
+            Instantiate(fadeOutPanel, Vector3.zero, Quaternion.identity);
+        }
+        yield return new WaitForSeconds(fadeWait);
+
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneToLoad);
+
+        while(!asyncOperation.isDone)
+        {
+            yield return null;
+        }
     }
 }
