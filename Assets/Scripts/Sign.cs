17c389332f5b6ea57c0a688af652c7b9300d3e31
Ian Doolan <iandoolan@gmail.com>
2020-10-18 22:21:21 +0100

    Part 29 Completed + Sign Fix
    2 collider issue

--- Assets/Scripts/Sign.cs
+++ Assets/Scripts/Sign.cs
@@ -34,7 +34,7 @@ public class Sign : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other) 
     {
-        if(other.CompareTag("Player"))
+        if(other.CompareTag("Player") && !other.isTrigger)
         {
             //Debug.Log("Player in range");
             playerInRange = true;
@@ -43,7 +43,7 @@ public class Sign : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D other) 
     {
-        if(other.CompareTag("Player"))
+        if(other.CompareTag("Player") && !other.isTrigger)
         {
             //Debug.Log("Player left range");
             playerInRange = false;
